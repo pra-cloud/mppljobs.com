6513d8cd8c4f02b19a58d94487a6898ba1655b41
Saaspect <80886214+saaspect@users.noreply.github.com>
2021-05-13 11:45:24 +0530

    commit2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-cd /var/www/html/pipelinetesting/
-c=$(ls -l /var/www/html/pipelinetesting/ | grep "^d" | wc -l)
+#cd /var/www/html/pipelinetesting/
+cd /var/www/html/mppljobs.com/
+c=$(ls -l /var/www/html/mppljobs.com/ | grep "^d" | wc -l)
 
-arr1=(Node React React)
-arr2=(index.js 3001 3002)
 
-arr=(/var/www/html/pipelinetesting/*/)
-for (( i=0; i<$c; i++ ))
+arr1=(/var/www/html/mppljobs.com/*/) 
+total=${!arr1[@]}
+for (( i=0; i<total; i++ ))
 do
-        cd ./${arr[i]}
-        if [ "${arr1[i]}" = "Node" ]
-        then
-                npm i
-                pm2 start ${arr2[i]}
-                cd ..
-        elif [ "${arr1[i]}" = "React" ]
-        then
-                npm i
-                npm run-script build
-                pm2 serve build ${arr2[i]} --spa
-                cd ..
-        fi
+	type=`jq -r .deployment_type ${arr1[$i]}/deploy.json`
+	if [ $type == react ]
+	then
+		portno=`jq -r  .port_number  ${arr1[$i]}/deploy.json`
+		cd ${arr[i]}
+		npm i
+		npm run-script build
+		pm2 serve build $portno --spa
+		cd
+	elif [ $type == node ]
+	then
+		filename=`jq -r  .port_number  ${arr1[$i]}/deploy.json`
+		cd ${arr[$i]}
+		npm i
+		pm2 start $filename
+		cd
+	else
+		echo "Not app folder"
+	fi
 done
